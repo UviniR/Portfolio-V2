29188a9dc256c5a57a7b75003751d733eb9a270b
UviniR <ranaweeraraua.19@uom.lk>
2023-08-13 17:35:11 +0530

    added projects and travels

--- data/projects.js
+++ data/projects.js
@@ -1,4 +1,7 @@
-import Image01 from "@public/read.svg"
+import Image01 from "@public/assets/projects/img1.png"
+import Image02 from "@public/assets/projects/img2.png"
+import Image03 from "@public/assets/projects/img3.png"
+
 
 const projects = [
     {
@@ -6,6 +9,22 @@ const projects = [
         desc: "Fine-tuned a pre-trained HuggingFace model for sentiment analysis of customer reviews received by a hotel",
         technologies: ["NLP", "PyTorch", "HuggingFace", "Streamlit"],
         img: Image01
-    }
+        // https://github.com/UviniR/Reviews-Sentiment-Analysis
+    },
+    {
+        title: "Web App for Online Quizzes",
+        desc: "Developed using Python, the web app allows teachers to create quizzes and students to attempt quizzes.",
+        technologies: ["Django", "MySQL", "HTML", "Bootstrap"],
+        img: Image02
+        // https://github.com/UviniR/Quiz-Time
+    },
+    {
+        title: "NYC Yellow Taxi Trips Analysis",
+        desc: "Runnning Python commands on a Apache Hadoop cluster to pre-process and analyse Big Data.",
+        technologies: ["GCP", "DataProc", "PySpark"],
+        img: Image03
+        // https://github.com/UviniR/NYC-Yellow-Taxi-Trips
+    },
+
 ]
 export default projects
