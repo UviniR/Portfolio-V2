114b17271531a6f02e6267044f15b1010e240fc1
UviniR <ranaweeraraua.19@uom.lk>
2023-08-14 14:18:03 +0530

    random fact update

--- components/Footer/footer.component.js
+++ components/Footer/footer.component.js
@@ -19,22 +19,25 @@ const Footer = () => {
         </div>
       </div>
 
-      <Modal show={show} onHide={handleClose} centered size="lg">
-        <Modal.Header closeButton>
-          <Modal.Title className="modal-title">8 Random Facts</Modal.Title>
+      <Modal show={show} onHide={handleClose} centered size="lg" className={`${styles.background}`}>
+        <Modal.Header closeButton  className={`${styles.card}`}>
+          <Modal.Title>
+            <span className={`px-2 ${styles.titles} rounded`}>8 RANDOM FACTS </span>
+            <p style={{ fontSize: '20px'}}> <b>Compiled with <span className="gradientText">my own judgments</span> + <span className="gradientText">what others say</span>.</b></p>
+            </Modal.Title>
         </Modal.Header>
 
-        <Modal.Body className="modal-body">Let me help you to picture me. Here're my own judgments + what others say about me.
-          <ol className="modal-body ol ">
-            <li>Life is all about "Perfect magical moments 💜"</li>
+        <Modal.Body className={`modal-body ${styles.card} px-5`}>
+          <ol className={`${styles.list}`}>
+            <li>Life is all about <b>"Perfect magical moments 💜"</b></li>
             <li>I am head over heels, a whopping 108% in love, love, loveeee with NLP even as a beginner!</li>
+            <li>I am famous for my OCD, especially when it comes to keeping things super organised.</li>
+            <li>Here's how my career dreams changed as I grew up: Air Hostess, Doctor, Film Director, Accountant, Academic Reseracher (can be a Doctor* again!!!)</li>
             <li>Sometimes I am struck by that flash energy to  binge-watch a TV series in just one day.</li>
-            <li>How my career dreams changed as I grew up: Film Director, Docoter, Air Hostess(cus I love to fly), Accountant, Academic Reseracher</li>
-            <li>Pretty skies and the serene sea never fail to fill me with sheer joy.</li>
-            <li>I have OCD, especially when it comes to keeping things super organised.</li>
-            <li>My folks say that I have a tad less common sense.</li>
+            <li>Matplotlib is like my classic favorite and Plotly is the new beat that gets my heart racing.</li>
+            <li>Folks say that I have a tad less common sense.</li>
             <li>What if I'm just pretending all along? <b>(Jinx!)</b></li>
-            <li>Andd here's a bonus: As for why eight? It is a very artistic number, and I love it.</li>
+            <li>Andd here's a bonus: As for why eight? Simply cus it is a very artistic number.</li>
           </ol>
 
         </Modal.Body>
